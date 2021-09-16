4d480babc1cff1bd675233ccb0e17f2233352608
numantariq435 <numantariq435@gmail.com>
2021-09-16 13:48:08 +0500

    useReducer & useContext

---
+++ src/components/context/ThemeContext.tsx
@@ -0,0 +1,16 @@
+import React, { createContext } from "react";
+import { theme } from "./theme";
+
+type ThemeContextProvideProps = {
+  children: React.ReactNode;
+};
+
+export const ThemeContext = createContext(theme);
+
+const ThemeContextProvider = ({ children }: ThemeContextProvideProps) => {
+  return (
+    <ThemeContext.Provider value={theme}> {children}</ThemeContext.Provider>
+  );
+};
+
+export default ThemeContextProvider;
